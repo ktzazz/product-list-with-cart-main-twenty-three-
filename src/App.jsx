16beac9ba7b6228183e787641eb8cf05363642c5
ktzazz <155502502+ktzazz@users.noreply.github.com>
2026-04-29 10:45:49 -0600

    partials scss

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,19 @@ function App() {
       <Filter />
       <ProductList />
       <Cart />
+      <footer className="attribution">
+        {" "}
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="https://www.frontendmentor.io/profile/ktzazz">Katia Aragón</a>.
+      </footer>
     </CartProvider>
   );
 }
--- src/productList.jsx
+++ src/productList.jsx
@@ -3,17 +3,16 @@ import { CartContext } from "./CartContext";
 import { Items } from "./ProductsCard";
 
 export const ProductList = () => {
-  // 1. Le pedimos al contexto la lista que YA PASÓ por los filtros
+  // Le pedimos al contexto la lista que YA PASÓ por los filtros
   const { filteredProducts } = useContext(CartContext);
 
   return (
     <div className="product-grid">
-      {/* 2. Mapeamos sobre la lista filtrada, no sobre el JSON original */}
+      {/* Mapeamos sobre la lista filtrada, no sobre el JSON original */}
       {filteredProducts.map((producto) => (
         <Items key={producto.name} product={producto} />
       ))}
 
-      {/* 3. Mensaje amigable si no hay resultados */}
       {filteredProducts.length === 0 && (
         <h3 className="no__results">Oops! No results</h3>
       )}
