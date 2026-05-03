48f28d6145a5ec7724ee9e8e03f72567dbb3fa1a
ktzazz <155502502+ktzazz@users.noreply.github.com>
2026-05-02 18:14:33 -0600

    comp filters

--- src/filter.jsx
+++ src/filter.jsx
@@ -15,29 +15,33 @@ export const Filter = () => {
 
   return (
     <div className="filter__container">
-      <h3>Categorías</h3>
-      {uniqueCategories.map((cat) => (
-        <label key={cat}>
-          <input
-            type="checkbox"
-            checked={tempCats.includes(cat)}
-            onChange={() => handleFilter(cat, "cat")}
-          />{" "}
-          {cat}
-        </label>
-      ))}
+      <div className="categories__cont">
+        <h3 className="filter__h3">Categorías</h3>
+        {uniqueCategories.map((cat) => (
+          <label key={cat}>
+            <input
+              type="checkbox"
+              checked={tempCats.includes(cat)}
+              onChange={() => handleFilter(cat, "cat")}
+            />{" "}
+            {cat}
+          </label>
+        ))}
+      </div>
 
-      <h3>Ingredientes</h3>
-      {uniqueIngredients.map((ing) => (
-        <label key={ing}>
-          <input
-            type="checkbox"
-            checked={tempIngs.includes(ing)}
-            onChange={() => handleFilter(ing, "ing")}
-          />{" "}
-          {ing}
-        </label>
-      ))}
+      <div className="ingredients__cont">
+        <h3 className="filter__h3">Ingredientes</h3>
+        {uniqueIngredients.map((ing) => (
+          <label key={ing}>
+            <input
+              type="checkbox"
+              checked={tempIngs.includes(ing)}
+              onChange={() => handleFilter(ing, "ing")}
+            />{" "}
+            {ing}
+          </label>
+        ))}
+      </div>
 
       <div className="filter-actions">
         <button onClick={applyFilters} className="btn-apply">
