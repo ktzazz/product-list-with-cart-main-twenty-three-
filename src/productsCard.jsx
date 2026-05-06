86b5a61273c3a021469a233b441b34ec350b0bd7
ktzazz <155502502+ktzazz@users.noreply.github.com>
2026-05-05 19:13:40 -0600

    product card after

--- src/productsCard.jsx
+++ src/productsCard.jsx
@@ -1,10 +1,17 @@
 import "./scss/productsCard.scss";
 import { BtnCart } from "./addCart";
+import { useContext } from "react";
+import { CartContext } from "./CartContext";
 
 export const Items = ({ product }) => {
+  const { cart } = useContext(CartContext);
+
+  const itemInCart = cart.find((item) => item.name === product.name);
+  const quantity = itemInCart ? itemInCart.quantity : 0;
+
   return (
     <div className="product__card">
-      <div className="pic__cont">
+      <div className={quantity === 0 ? "pic__cont" : "after"}>
         <picture>
           <source
             media="(min-width: 1024px)"
