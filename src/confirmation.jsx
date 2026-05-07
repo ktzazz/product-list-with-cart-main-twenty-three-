e6e99b8d9a0acca531af1c5971f1717e067872a6
ktzazz <155502502+ktzazz@users.noreply.github.com>
2026-05-07 15:20:19 -0600

    confirmation container

--- src/confirmation.jsx
+++ src/confirmation.jsx
@@ -1,4 +1,5 @@
 import "./scss/confirmation.scss";
+import { useEffect } from "react";
 import { useContext, useState } from "react";
 import { CartContext } from "./CartContext";
 
@@ -7,6 +8,17 @@ export const Confirmation = ({ total }) => {
 
   const [btnConfirm, setBtnConfirm] = useState(false);
 
+  useEffect(() => {
+    if (btnConfirm) {
+      document.body.classList.add("no_scroll");
+    } else {
+      document.body.classList.remove("no_scroll");
+    }
+
+    // Por si el componente se desmonta de golpe
+    return () => document.body.classList.remove("no_scroll");
+  }, [btnConfirm]); // Se ejecuta cada vez que btnConfirm cambie
+
   return (
     <div className="btn__confirm">
       {!btnConfirm ? (
@@ -14,40 +26,56 @@ export const Confirmation = ({ total }) => {
           Confirm Order
         </button>
       ) : (
-        <div className="confirm__container">
-          <img src="/images/icon-order-confirmed.svg" alt="order_confirmed" />
-          <h2 className="confirm__title">Order Confirmed</h2>
-          <p className="confirm__txt">We hope you enjoy your food!</p>
-          <div className="confirm__products">
-            {cart.map((item, index) => (
-              <div className="confirm__div" key={`${item.name}-${index}`}>
-                <img src={item.image.thumbnail} alt={item.name} />
+        <>
+          <button className="cnfrm__btn">Confirm Order</button>
+          <div className="overlay"></div>
+          <div className="confirm__container">
+            <img
+              className="confirm__img"
+              src="/images/icon-order-confirmed.svg"
+              alt="order_confirmed"
+            />
+            <h2 className="confirm__title">Order Confirmed</h2>
+            <p className="confirm__txt">We hope you enjoy your food!</p>
+            <div className="confirm__products">
+              {cart.map((item, index) => (
+                <div className="confirm__div" key={`${item.name}-${index}`}>
+                  <img
+                    className="map__img"
+                    src={item.image.thumbnail}
+                    alt={item.name}
+                  />
 
-                <div className="confirm__items">
-                  <h4>{item.name}</h4>
-                  <span className="confirm__qty">{item.quantity}x</span>
-                  <span className="confirm__price">
-                    @{""}
-                    {item.price}
-                  </span>
+                  <div className="confirm__items">
+                    <h4>{item.name}</h4>
+                    <div className="span__order">
+                      <span className="confirm__qty">{item.quantity}x</span>
+                      <span className="confirm__price">
+                        @{""}${item.price}
+                      </span>
+                    </div>
+                  </div>
+                  <p className="confirm__total">
+                    ${item.price * item.quantity}
+                  </p>
                 </div>
-                <p className="confirm__total">{item.price * item.quantity}</p>
+              ))}
+              <div className="order__pay">
+                <span className="pay__txt">Order Total</span>
+                <span className="pay__total">${total}</span>
               </div>
-            ))}
-            <div className="order__pay">
-              <span className="pay__txt">Order Total</span>
-              <span className="pay__total">${total}</span>
             </div>
+            <button
+              className="cnfrm__btn"
+              onClick={() => {
+                setBtnConfirm(false);
+                clearCart();
+              }}
+            >
+              Start New Order
+            </button>
           </div>
-          <button
-            onClick={() => {
-              setBtnConfirm(false);
-              clearCart();
-            }}
-          >
-            Start New Order
-          </button>
-        </div>
+        </>
       )}
     </div>
   );
